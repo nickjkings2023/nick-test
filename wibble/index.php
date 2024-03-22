e3d2a5447bad1f27f719dec38c8eddf16fd434fd
nkings <nkings@D2L-5yB8ceObbuS.desire2learn.d2l>
2024-03-22 10:09:45 +0000

    bloop

---
+++ wibble/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+ Course Merchant
+ D2L Europe
+ Copyright 2010-2023
+ NJK
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+	<?php
+	// put your code here
+	?>
+    </body>
+</html>
